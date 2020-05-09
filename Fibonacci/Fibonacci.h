326d9db62fcdc99286d6cf95b35b5a25bfab0780
Gulya Akhtyamova <gulya.nadym@gmail.com>
2020-05-09 20:47:01 +0300

    Initial Commit

---
+++ Fibonacci/Fibonacci.h
@@ -0,0 +1,19 @@
+//
+//  Fibonacci.h
+//  Fibonacci
+//
+//  Created by Gulya Boiko on 5/9/20.
+//  Copyright © 2020 com.gulya.boiko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Fibonacci.
+FOUNDATION_EXPORT double FibonacciVersionNumber;
+
+//! Project version string for Fibonacci.
+FOUNDATION_EXPORT const unsigned char FibonacciVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fibonacci/PublicHeader.h>
+
+
